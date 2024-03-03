c8851cd787fd62c0125124c2adb72f3fdf0192a2
Manita Mao <manita.mao@etu.univ-lyon1.fr>
2024-03-03 14:32:40 +0100

    nouveau script déplacement aléatoire qui marche je dois ajouter les commentaires et la vidéo que j'ai suivi

--- unity_project/Assets/Scripts/randomWalk.cs
+++ unity_project/Assets/Scripts/randomWalk.cs
@@ -1,30 +1,126 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Profiling.Memory.Experimental;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class randomWalk : MonoBehaviour
+public enum AnimalState
 {
+    Idle,
+    Moving,
+}
 
+[RequireComponent(typeof(NavMeshAgent))]
 
-    // Start is called before the first frame update
+public class Animal : MonoBehaviour
+{
+    [Header("Wander")] //distance max 
+    public float wanderDistance = 50f;
+    public float walkSpeed = 5f;
+    public float maxWalkTime = 6f;
 
-    public float m_Range = 25.0f;
-    public NavMeshAgent m_Agent;
-    void Start()
+    [Header("Idle")] // pause de l'animal
+    public float idleTime = 5f;
+
+    protected NavMeshAgent navMeshAgent;
+    protected AnimalState currentState  = AnimalState.Idle;
+
+    private void Start()
     {
-        m_Agent = GetComponent<NavMeshAgent>();
+        InitialiseAnimal();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected virtual void InitialiseAnimal()
     {
-        if (m_Agent.pathPending || m_Agent.remainingDistance > 0.1f)
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.speed = walkSpeed;
+
+        currentState = AnimalState.Idle;
+        UpdateState();
+
+    }
+
+    protected virtual void UpdateState()
+    {
+        switch (currentState)
         {
-            return;
+            case AnimalState.Idle:
+                HandleIdleState();
+                break;
+            case AnimalState.Moving:
+                HandleMovingState();
+                break;
+        }
+    }
+
+    protected Vector3 GetRandomNavMeshPosition(Vector3 origin, float distance)
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * distance; // point random
+        randomDirection += origin;
+        NavMeshHit navMeshHit;
+
+        if(NavMesh.SamplePosition(randomDirection,out navMeshHit, distance, NavMesh.AllAreas))
+        {
+            return navMeshHit.position;
+        }
+        else
+        {
+            return GetRandomNavMeshPosition(origin, distance);
+        }
+    }
+
+
+    protected virtual void HandleIdleState()
+    {
+        StartCoroutine(WaitToMove());
+    }
+
+    private IEnumerator WaitToMove()
+    {
+        float waitTime = Random.Range(idleTime / 2, idleTime * 2);
+        yield return new WaitForSeconds(waitTime);
+
+        Vector3 randomDestination = GetRandomNavMeshPosition(transform.position, wanderDistance);
+
+        navMeshAgent.SetDestination(randomDestination);
+        SetState(AnimalState.Moving);
+    }
+
+    protected virtual void HandleMovingState()
+    {
+        StartCoroutine(WaitToReachDestination());
+    }
+
+    private IEnumerator WaitToReachDestination()
+    {
+        float startTime = Time.time;
+
+        while(navMeshAgent.remainingDistance > navMeshAgent.stoppingDistance)
+        {
+            if(Time.time - startTime >= maxWalkTime)
+            {
+                navMeshAgent.ResetPath();
+                SetState(AnimalState.Idle);
+                yield break;
+            }
+
+            yield return null;
         }
 
-        m_Agent.destination = m_Range * Random.insideUnitCircle;
+        SetState(AnimalState.Idle);
     }
 
+    protected void SetState(AnimalState newState)
+    {
+        if(currentState == newState) return;
+
+        currentState = newState;
+        OnStateChange(newState);
+    }
+
+    protected virtual void OnStateChange(AnimalState newState)
+    {
+        UpdateState();
+    }
 }
